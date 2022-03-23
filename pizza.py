12bbfb0450c0a47f535ae4e57176215bd39925d6
TyLOOIOIOOI <Ty_Liu1@baylor.edu>
2022-03-23 15:35:41 -0500

    HW CHAPTER 13

---
+++ pizza.py
@@ -0,0 +1,49 @@
+import tkinter
+
+class pizzaGUI:
+    def __init__(self):
+        self.mainWindow = tkinter.Tk()
+        self.toppingsCost = 0
+        self.crustCost = 0
+
+
+    #def setToppingsPrice(self):
+        #print self.toppingsCost = 
+
+# window = tkinter.Tk()
+# tkinter.Label(window, text="Buy a Pizza!").grid(column=0, row=0)
+# tkinter.Label(window, text="Choose your toppings!").grid(column=0, row=1)
+# tkinter.Checkbutton(window, text="Pepperoni ($1.00)" command=toppingPrice).grid(column=0, row=2)
+# tkinter.Checkbutton(window, text="Chicken").grid(column=1, row=2)
+# tkinter.Checkbutton(window, text="Mushrooms").grid(column=2, row=2)
+# crustCost = 0
+# tkinter.Label(window, text="Choose your crust!").grid(column=0, row=3)
+# tkinter.Radiobutton(window, text="Normal ($1.00)").grid(column=0, row=4)
+# tkinter.Radiobutton(window, text="Cheesy ($2.00)").grid(column=1, row=4)
+# #tkinter.Button(window, text="Quit", command=window.destroy).grid(column=1, row=0)
+# #tkinter.Button(window, text="Quit", command=window.destroy).grid(column=2, row=2)
+# #tkinter.Button(window, text="Quit", command=window.destroy).grid(column=0, row=3)
+
+# # Code to add widgets will go here...
+# window.mainloop()
+
+# 0,0     1,0     2,0
+# 0,1     1,1     2,1
+# 0,2     1,2     2,2
+
+
+#Frames
+#Labels
+#input box
+#buttons
+#radio buttons
+# check boxes
+# You can use check boxes for for selecting toppings (each with a different cost), 
+# radio buttons for the type of crust selected (each with a different cost), 
+# buttons for calculation and quit. 
+# The input box can be used to record the name of the person placing the order. 
+# Use a message box to display the total cost of the pizza along with the name of 
+# the person placing the order.
+# Make sure your design is well laid out and intuitive to the user. 
+# Take account of spacing and packing so that everything is properly aligned and professional looking. 
+# Be creative with font, color, size, etc.
